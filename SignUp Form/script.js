19706120085514ef8d4e54cdce1ec3bdb0876d19
Momina Farooq <107984079+Momina-Farooq-045247@users.noreply.github.com>
2022-08-16 11:46:10 -0700

    Add files via upload

---
+++ SignUp Form/script.js
@@ -0,0 +1,3 @@
+const container = document.querySelector('.container');
+const signUpBtn = document.querySelector(".green-bg button");
+signUpBtn.addEventListener("click",()=>{container.classList.toggle("change");});
